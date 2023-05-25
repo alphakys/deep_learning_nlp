0b2513bb59c3d36cebf193417c59bb4bb7b3aa65
alphakys <gys1120@naver.com>
2023-05-25 22:02:41 +0900

    로지스틱 회귀 실습도 완료git add -A ㅋ

--- test.py
+++ test.py
@@ -21,20 +21,35 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_breast_cancer
+from sklearn.metrics import accuracy_score
 
 
+cancer_data = load_breast_cancer()
 
-ads_pd = pd.read_csv('Social_Network_Ads.csv')
-ads_pd['Gender'] = ads_pd['Gender'].transform(lambda x: float(1) if x == 'Male' else float(0))
-
-X = ads_pd.iloc[:, 1:-1]
-y = ads_pd.iloc[:, -1]
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
-
-ss = StandardScaler()
-# processed_X = ss.fit_transform(X_train)
+cancer_df = pd.DataFrame(data=cancer_data['data'], columns=cancer_data['feature_names'])
+y = cancer_data['target']
+X_train, X_test, y_train, y_test = train_test_split(cancer_df, y, random_state=0)
 
 lr = LogisticRegression()
+lr.fit(X_train, y_train)
+
+prd = lr.predict(X_test)
+score = accuracy_score(y_test, prd)
+
+
+
+#
+# ads_pd = pd.read_csv('Social_Network_Ads.csv')
+# ads_pd['Gender'] = ads_pd['Gender'].transform(lambda x: float(1) if x == 'Male' else float(0))
+#
+# X = ads_pd.iloc[:, 1:-1]
+# y = ads_pd.iloc[:, -1]
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+#
+# ss = StandardScaler()
+# # processed_X = ss.fit_transform(X_train)
+#
+# lr = LogisticRegression()
 
 # x_train = x_train.values.reshape(-1, 1)
 # x_test = x_test.values.reshape(-1, 1)
