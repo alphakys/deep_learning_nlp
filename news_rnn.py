db50c78b683d59411a94d27926a0bd55364a580e
alphakys <gys1120@naver.com>
2023-06-06 22:59:15 +0900

    Embedding layer 위키독스 가지고 학습 내가 스스로 만든 모델이랑 pre porcessing 단 및 모델 구성에서 많은 차이를 보였으나 구현은 함git add -A

---
+++ news_rnn.py
@@ -0,0 +1,38 @@
+import os
+import warnings
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+warnings.simplefilter(action='ignore', category=FutureWarning)
+import numpy as np
+import pandas as pd
+
+from string import punctuation
+
+from keras.optimizers import Adam
+from keras.preprocessing.text import Tokenizer
+from keras.utils import pad_sequences, to_categorical
+from keras.layers import Dense, Embedding, LSTM
+
+news_pd: pd.DataFrame = pd.read_csv('ArticlesJan2018.csv')
+
+headline_list = news_pd['headline'].tolist()
+
+tk = Tokenizer()
+tk.fit_on_texts(headline_list)
+
+vocab_size = len(tk.word_index) + 1
+
+sequences = []
+for line in headline_list:
+    sequence = tk.texts_to_sequences([line])[0]
+    sequences.append(sequence)
+
+encoded_list = []
+for seq in sequences[:3]:
+    cnt = len(seq)
+    for i in range(2, cnt):
+        encoded_list.append(seq[:i])
+
+
+
+max_len = max([len(seq) for seq in sequences])
--- rnn_practice2.py
+++ rnn_practice2.py
@@ -1,17 +1,85 @@
 import os
 import warnings
 
-import numpy as np
+from keras.optimizers import Adam
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
-from keras.preprocessing.text import Tokenizer
-from keras_preprocessing.sequence import pad_sequences
-from keras.utils import to_categorical
+import numpy as np
 
-texts = """
-경마장에 있는 말이 뛰고 있다
-그의 말이 법이다
-가는 말이 고와야 오는 말이 곱다
-"""
+from keras.preprocessing.text import Tokenizer
+from keras.utils import pad_sequences, to_categorical
+
+texts = """경마장에 있는 말이 뛰고 있다\n그의 말이 법이다\n가는 말이 고와야 오는 말이 곱다"""
+
+tk = Tokenizer()
+tk.fit_on_texts([texts])
+vocab_size = len(tk.word_index) + 1
+
+sequences = []
+for line in texts.split('\n'):
+    print(line)
+    encoded = tk.texts_to_sequences([line])[0]
+
+    for i in range(1, len(encoded)):
+        sequence = encoded[:i + 1]
+        sequences.append(sequence)
+
+# print('sequences: ', sequences)
+
+max_len = max([len(l) for l in sequences])
+sequences = pad_sequences(sequences, maxlen=max_len, padding='pre')
+
+X = sequences[:, :-1]
+y = sequences[:, -1]
+
+y = to_categorical(y, num_classes=vocab_size)
+
+from keras.models import Sequential
+from keras.layers import Embedding, Dense, SimpleRNN
+
+# X의 argument를 보면 1 ~ 10까지의 index가 있기 때문에 input_dim을 10차원으로 설정한다.
+embedding_dim = 10
+hidden_units = 32
+
+model = Sequential()
+# [STUDY] Embedding layer에 대해서 더 공부 필요
+model.add(Embedding(vocab_size, embedding_dim))
+model.add(SimpleRNN(hidden_units))
+model.add(Dense(vocab_size, activation='softmax'))
+model.compile(optimizer=Adam(learning_rate=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
+
+model.fit(X, y, epochs=200, verbose=2)
+
+def sentence_gerneration(model, tk, current_word, n):
+    init_word = current_word
+    sentence = ''
+
+    # n번 반복
+    for _ in range(n):
+        encoded = tk.texts_to_sequences([current_word])[0]
+        encoded = pad_sequences([encoded], maxlen=5, padding='pre')
+        result = model.predict(encoded, verbose=0)
+        result = np.argmax(result, axis=1)
+
+        for word, index in tk.word_index.items():
+            # 만약 예측한 단어와 인덱스와 동일한 단어가 있다면 break
+            if index == result:
+                break
+
+            # 현재 단어 + ' ' + 예측 단어를 현재 단어로 변경
+        current_word = current_word + ' ' + word
+
+        # 예측 단어를 문장에 저장
+        sentence = sentence + ' ' + word
+
+
+    sentence = init_word + sentence
+    return sentence
+
+
+
+
+
+
