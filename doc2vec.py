ad98f07cec81b0d826fe61788894b91773ed4d1e
alphakys <gys1120@naver.com>
2023-06-24 17:53:56 +0900

    23.6.24

---
+++ doc2vec.py
@@ -0,0 +1,19 @@
+import os
+import warnings
+
+import numpy as np
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+import pandas as pd
+
+from konlpy.tag import Mecab
+from gensim.models.doc2vec import TaggedDocument
+from tqdm import tqdm
+
+df = pd.read_csv('~/문서/dart.csv')
+for k, v in enumerate(df.iterrows()):
+    if k <10:
+        TaggedDocument
+        print(k,v[1][['name', 'business']])
