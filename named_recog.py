6d20bd4ab3c80fc20094728bda1648b340bde3a8
alphakys <gys1120@naver.com>
2023-07-08 14:20:57 +0900

    23.7.8

--- named_recog.py
+++ named_recog.py
@@ -21,105 +21,42 @@ tagged_sentences = []
 sentence = []
 
 for line in f:
-    if len(line)==0 or line.startswith('-DOCSTART') or line[0]=="\n":
+    if len(line) == 0 or line.startswith('-DOCSTART') or line[0] == "\n":
         if len(sentence) > 0:
             tagged_sentences.append(sentence)
             sentence = []
         continue
-    splits = line.split(' ') # 공백을 기준으로 속성을 구분한다.
-    splits[-1] = re.sub(r'\n', '', splits[-1]) # 줄바꿈 표시 \n을 제거한다.
-    word = splits[0].lower() # 단어들은 소문자로 바꿔서 저장한다.
-    sentence.append([word, splits[-1]]) # 단어와 개체명 태깅만 기록한다.
-
-tagged_sentences
-for sen in tagged_sentences:
-    tmp_sen = list(zip(*sen))
+    splits = line.split(' ')  # 공백을 기준으로 속성을 구분한다.
+    splits[-1] = re.sub(r'\n', '', splits[-1])  # 줄바꿈 표시 \n을 제거한다.
+    word = splits[0].lower()  # 단어들은 소문자로 바꿔서 저장한다.
+    sentence.append([word, splits[-1]])  # 단어와 개체명 태깅만 기록한다.
 
 sentences, ner_tags = [], []
+for tagged_sentence in tagged_sentences:  # 14,041개의 문장 샘플을 1개씩 불러온다.
+    sentence, tag_info = zip(*tagged_sentence)  # 각 샘플에서 단어들은 sentence에 개체명 태깅 정보들은 tag_info에 저장.
+    sentences.append(list(sentence))  # 각 샘플에서 단어 정보만 저장한다.
+    ner_tags.append(list(tag_info))  # 각 샘플에서 개체명 태깅 정보만 저장한다.
 
-for sen in tagged_sentences:
-    tmp_sen = list(zip(*sen))
-    sentences.append(tmp_sen[0])
-    ner_tags.append(tmp_sen[1])
+# 단어 사이즈는 상위 4000개를 사용한다.
+vocab_size = 4000
+src_tokenizer = Tokenizer(num_words=vocab_size, oov_token='OOV')
+src_tokenizer.fit_on_texts(sentences)
 
-sentences[0]
-sentences[3]
-ner_tags[2]
-len(sentences)
-len(ner_tags)
-len_list = [len(sen) for sen in sentences]
-ner_tk = Tokenizer()
-sentences, ner_tags = [], []
+tar_tokenizer = Tokenizer()
+tar_tokenizer.fit_on_texts(ner_tags)
 
-for sen in tagged_sentences:
-    tmp_sen = list(zip(*sen))
-    sentences.append(list(tmp_sen[0]))
-    ner_tags.append(list(tmp_sen[1]))
+tag_size = len(tar_tokenizer.word_index) + 1
 
-del sentence_tk
-sentence_tk = Tokenizer()
-sentence_tk.fit_on_texts(sentences)
-sentence_tk.word_index
-vocab_size = len(sentence_tk.word_index) + 1
-vocab_size
-len_list
-max(len_list)
-sum(len_list) / len(len_list)
-int(sum(len_list) / len(len_list))
-avr_len = 14
-plt.hist(len_list, bins=50)
-plt.show()
-sentences[1]
-sen_tokens = sentence_tk.texts_to_sequences(sentences)
-sen_tokens[1]
-sen_tokens[3]
-padded_sequences = pad_sequences(sen_tokens)
-padded_sequences = pad_sequences(sen_tokens, maxlen=avr_len)
-padded_sequences
-padded_sequences.shape
-ner_tk.fit_on_texts(ner_tags)
-ner_tk.index_word
-ner_tags[1]
-ner_tags[3]
-tag_size = len(ner_tk.index_word) + 1
-ner_tokens = ner_tk.texts_to_sequences(ner_tags)
-ner_tokens[1]
-padded_sequences = pad_sequences(sen_tokens, maxlen=avr_len, padding='post')
-padded_sequences[3]
-padded_sequences[4]
-padded_sequences[5]
-padded_sequences[10]
-padded_sequences[11]
-padded_sequences[12]
-ner_tokens
-padded_labels = pad_sequences(ner_tokens, maxlen=avr_len)
-padded_labels
-processed_labels = to_categorical(ner_tokens)
-processed_labels = to_categorical(padded_labels)
-processed_labels
-X_train, X_test, y_train, y_test = train_test_split(padded_sequences, processed_labels, test_size=0.25,
-                                                    random_state=1004)
-X_train, X_test, y_train, y_test = train_test_split(padded_sequences, processed_labels, test_size=0.25,
-                                                    random_state=1004)
-from keras.models import Sequential
-from keras.layers import Dense, Embedding, LSTM, Bidirectional, TimeDistributed
-from keras.optimizers import Adam
+X_train = src_tokenizer.texts_to_sequences(sentences)
+y_train = tar_tokenizer.texts_to_sequences(ner_tags)
 
-embedding_dim = 128
-hidden_units = 128
-model.add(Embedding(vocab_size, embedding_dim, input_length=avr_len))
-model.add(Bidirectional(LSTM(hidden_units, return_sequences=True, activation='relu')))
-del model
+index_to_word = src_tokenizer.index_word
+index_to_ner = tar_tokenizer.index_word
+
 model = Sequential()
 model.add(Embedding(vocab_size, embedding_dim, input_length=avr_len, mask_zero=True))
 model.add(Bidirectional(LSTM(hidden_units, return_sequences=True)))
 model.add(TimeDistributed(Dense(tag_size, activation='softmax')))
 model.compile(loss='categorical_crossentropy', optimizer=Adam(0.001), metrics=['accuracy'])
 model.fit(X_train, y_train, batch_size=128, epochs=8, validation_data=(X_test, y_test))
-X_test[0]
-model.predict(X_test[1])
-X_test
-X_test.shape
-X_test[1]
-model.summary()
-tag_size
+
